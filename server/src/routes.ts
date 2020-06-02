2927790fe96115a34d40ab6babb7ba6b214d1cff
henbalmant <henrique.balmant@gmail.com>
2020-06-02 15:18:21 -0300

    Add create collect_points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -8,12 +8,52 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             name: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
         };
     })
 
     return response.json(serializedItems);
+});
+
+routes.post('/collect_points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items,
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('collect_points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    });
+
+    const collect_points_id = insertedIds[0];
+
+    const collectPointItems = items.map((items_id: number) => {
+        return {
+            items_id,
+            collect_points_id,
+        };
+    });
+
+    await trx('collect_points_items').insert(collectPointItems);
+
+    return response.json({ success: true });
 });
 
 export default routes;
