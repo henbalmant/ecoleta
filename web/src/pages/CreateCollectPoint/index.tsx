ccfa2aa2b4afe16b52580f39d6eb9719d6df592c
henbalmant <henrique.balmant@gmail.com>
2020-06-03 23:35:23 -0300

    Add map center to current location

--- web/src/pages/CreateCollectPoint/index.tsx
+++ web/src/pages/CreateCollectPoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, ChangeEvent } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 import axios from 'axios';
 
@@ -27,8 +28,19 @@ const CreateCollectPoint = () => {
     const [ufs, setUfs] = useState<string[]>([]);
     const [cities, setCities] = useState<string[]>([]);
 
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
+
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+
+            setInitialPosition([latitude, longitude]);
+        });
+    }, []);
 
     useEffect(() => {
         api.get('items').then(response => {
@@ -66,6 +78,13 @@ const CreateCollectPoint = () => {
         const city = event.target.value;
 
         setSelectedUf(city);
+    }
+
+    function handleMapClick(event: LeafletMouseEvent) {
+        setSelectedPosition([
+            event.latlng.lat,
+            event.latlng.lng,
+        ]);
     }
 
     return(
@@ -123,13 +142,13 @@ const CreateCollectPoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
-                    <Map center={[-19.9224853, -43.9913397]} zoom={15}>
+                    <Map center={initialPosition} zoom={15} onClick={handleMapClick} >
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
 
-                        <Marker position={[-19.9224853, -43.9913397]} />
+                        <Marker position={selectedPosition} />
                     </Map>
 
                     <div className="field-group">
