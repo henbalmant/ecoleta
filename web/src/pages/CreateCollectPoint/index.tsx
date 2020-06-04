f2253b8bb47694420d5c794f4164f70459c08e4e
henbalmant <henrique.balmant@gmail.com>
2020-06-03 22:19:33 -0300

    Add map to create collect point form

--- web/src/pages/CreateCollectPoint/index.tsx
+++ web/src/pages/CreateCollectPoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -60,6 +61,15 @@ const CreateCollectPoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-19.9224853, -43.9913397]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-19.9224853, -43.9913397]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
