aefe8a88f5a77b38698c5486a8027522da1ffaa1
henbalmant <henrique.balmant@gmail.com>
2020-06-10 01:17:32 -0300

    Add react-dropzone to upload images

--- web/src/pages/CreateCollectPoint/index.tsx
+++ web/src/pages/CreateCollectPoint/index.tsx
@@ -6,6 +6,8 @@ import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 import axios from 'axios';
 
+import Dropzone from '../../components/Dropzone'
+
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
@@ -40,6 +42,7 @@ const CreateCollectPoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -123,15 +126,19 @@ const CreateCollectPoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+
+        if (selectedFile) {
+            data.append('image', selectedFile);
         }
 
         await api.post('collect_points', data);
@@ -154,6 +161,8 @@ const CreateCollectPoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
